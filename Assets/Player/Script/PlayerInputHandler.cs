24c39cd91e6d94731601c4748e2c9b9a42bb1f29
theo-bsl <t.basile@lahorde.school>
2025-07-17 11:18:23 +0200

    player movement and rotation

---
+++ Assets/Player/Script/PlayerInputHandler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Player.Script
+{
+    public class PlayerInputHandler : MonoBehaviour
+    {
+        [SerializeField] private PlayerMovement playerMovement;
+        
+        public void ReceiveMovementInput(InputAction.CallbackContext context)
+        {
+            playerMovement.Move(context.ReadValue<Vector2>());
+        }
+    }
+}
---
+++ Assets/Player/Script/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Player.Script
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class PlayerMovement : MonoBehaviour
+    {
+        public float moveSpeed = 5f;
+        public float rotationSpeed = 10f;
+
+        private Vector2 moveInput;
+        private Vector3 direction;
+        private Rigidbody rb;
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        public void Move(Vector2 input)
+        {
+            moveInput = input;
+        }
+
+        private void FixedUpdate()
+        {
+            direction.Set(moveInput.x, 0f, moveInput.y);
+
+            if (direction.sqrMagnitude > 0.01f)
+            {
+                Quaternion toRotation = Quaternion.LookRotation(direction, Vector3.up);
+                transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, rotationSpeed * Time.fixedDeltaTime);
+
+                rb.MovePosition(rb.position + direction.normalized * (moveSpeed * Time.fixedDeltaTime));
+            }
+        }
+    }
+
+}

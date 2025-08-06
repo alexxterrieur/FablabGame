9f68ea35cc437aa243888a12eb7cf2d8bc1d5db6
shenanigan11 <enzo2dessaint@gmail.com>
2025-08-06 14:32:53 +0200

    glow

--- Assets/Highlight/Shader/Highlight.cs
+++ Assets/Highlight/Shader/Highlight.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Highlight : MonoBehaviour
@@ -6,7 +7,10 @@ public class Highlight : MonoBehaviour
     [SerializeField]
     private Renderer renderer;
     [SerializeField]
-    private Color color = Color.white;
+    public Color glowColor = Color.cyan;
+    public Color noGlowColor = Color.black;
+    public float glowSpeed = 2f;
+    private Coroutine glowCoroutine;
 
     [SerializeField] Color wrongColor = Color.red;
 
@@ -23,27 +27,47 @@ public class Highlight : MonoBehaviour
 
     public void ToggleHighlight(HighlightState state)
     {
-        switch(state)
+        switch (state)
         {
-            case HighlightState.Interactable :
-                //We need to enable the EMISSION
-                material.EnableKeyword("_EMISSION");
-                //before we can set the color
-                material.SetColor("_EmissionColor", color);
+            case HighlightState.Interactable:
+                // Start glow animation
+                if (glowCoroutine != null)
+                    StopCoroutine(glowCoroutine);
+
+                glowCoroutine = StartCoroutine(GlowPulseCoroutine());
                 break;
 
-            case HighlightState.NotInteractable :
+            case HighlightState.NotInteractable:
+                if (glowCoroutine != null)
+                    StopCoroutine(glowCoroutine);
+
                 material.EnableKeyword("_EMISSION");
                 material.SetColor("_EmissionColor", wrongColor);
                 break;
 
             default:
-                //we can just disable the EMISSION
-                //if we don't use emission color anywhere else
+                if (glowCoroutine != null)
+                    StopCoroutine(glowCoroutine);
+
                 material.DisableKeyword("_EMISSION");
                 break;
         }
     }
+
+    private IEnumerator GlowPulseCoroutine()
+    {
+        material.EnableKeyword("_EMISSION");
+
+        float t = 0f;
+        while (true)
+        {
+            t += Time.deltaTime * glowSpeed;
+            Color lerpedColor = Color.Lerp(noGlowColor, glowColor, (Mathf.Sin(t) + 1f) / 2f);
+            material.SetColor("_EmissionColor", lerpedColor);
+            yield return null;
+        }
+    }
+
 
     public enum HighlightState
     {

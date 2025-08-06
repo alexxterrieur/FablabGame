a55691dafc2633a0474bb92efb414718a9060eab
theo-bsl <t.basile@lahorde.school>
2025-08-06 11:46:12 +0200

    speed increase

--- Assets/Mini-Game/Milling_machine/Scripts/Reamer.cs
+++ Assets/Mini-Game/Milling_machine/Scripts/Reamer.cs
@@ -5,10 +5,27 @@ public class Reamer : MonoBehaviour
     public bool isReamerDown = false;
     private MillingButton currentMillingButton;
     [SerializeField] private float drillingSpeed = 5f;
+    
+    private float currentDrillingSpeed = 0f;
+
+    private void Start()
+    {
+        currentDrillingSpeed = drillingSpeed;
+    }
 
     public void StartDrilling(float _drillingSpeed)
     {
         drillingSpeed = _drillingSpeed;
+    }
+    
+    public void IncreaseReamerSpeed(float speedMultiplier)
+    {
+        currentDrillingSpeed *= speedMultiplier;
+    }
+
+    public void ResetReamerSpeed()
+    {
+        currentDrillingSpeed = drillingSpeed;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -16,8 +33,7 @@ public class Reamer : MonoBehaviour
         if(other.TryGetComponent(out MillingButton millingButton) && !isReamerDown)
         {
             currentMillingButton = millingButton;
-            millingButton?.OnStartDrilling(drillingSpeed);
-            Debug.Log("StartDrilling");
+            millingButton?.OnStartDrilling(currentDrillingSpeed);
         }
     }
 
@@ -27,7 +43,6 @@ public class Reamer : MonoBehaviour
         {
             currentMillingButton?.OnStopDrilling(currentMillingButton);
             currentMillingButton = null;
-            Debug.Log("StopDrilling");
         }
     }
 }

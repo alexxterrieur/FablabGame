297ea779293b133b32605b06a7c4b89102528a02
alexisgodbillot <114652313+alexxterrieur@users.noreply.github.com>
2025-08-07 10:25:35 +0200

    fix camera movement

--- Assets/Player/Script/CameraMovement.cs
+++ Assets/Player/Script/CameraMovement.cs
@@ -7,11 +7,9 @@ public class CameraMovement : MonoBehaviour
     [SerializeField] private float playerZMax = 8f;
     [SerializeField] private float camZMin = -19f;
     [SerializeField] private float camZMax = -12.7f;
-    [SerializeField] private float moveSpeed = 5f;
 
     private float fixedX;
     private float fixedY;
-    private float lastPlayerZ;
 
     private CameraCraftMovement cameraCraftMovement;
 
@@ -19,8 +17,6 @@ public class CameraMovement : MonoBehaviour
     {
         fixedX = transform.position.x;
         fixedY = transform.position.y;
-        if (target != null)
-            lastPlayerZ = target.position.z;
 
         cameraCraftMovement = GetComponent<CameraCraftMovement>();
     }
@@ -34,18 +30,10 @@ public class CameraMovement : MonoBehaviour
 
         float currentPlayerZ = target.position.z;
 
-        if (Mathf.Approximately(currentPlayerZ, lastPlayerZ))
-        {
-            return;
-        }
-
-        lastPlayerZ = currentPlayerZ;
-
         float clampedPlayerZ = Mathf.Clamp(currentPlayerZ, playerZMin, playerZMax);
         float t = (clampedPlayerZ - playerZMin) / (playerZMax - playerZMin);
         float targetCamZ = Mathf.Lerp(camZMin, camZMax, t);
-        float newCamZ = Mathf.MoveTowards(transform.position.z, targetCamZ, moveSpeed * Time.deltaTime);
 
-        transform.position = new Vector3(fixedX, fixedY, newCamZ);
+        transform.position = new Vector3(fixedX, fixedY, targetCamZ);
     }
 }
